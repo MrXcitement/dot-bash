d2cac00def048495b075dd5d6da077204c2c3ab8
Mike Barker <mike@thebarkers.com>
2014-07-10 10:04:06 -0600

    Added support for java environment manager jenv.

---
+++ bash_profile.d/jenv.sh
@@ -0,0 +1,5 @@
+# jenv.sh -- setup the jenv command line tool
+#            used to help you forget how to set the JAVA_HOME environment variable
+#            http://www.jenv.be/
+
+if which jenv > /dev/null; then eval "$(jenv init -)"; fi
