a0ea3ef591098ae7875161e151067803591410d2
Mike Barker <mike@thebarkers.com>
2014-10-24 12:08:35 -0600

    Added sshagent.sh

--- bash_logout
+++ bash_logout
@@ -1,5 +1,12 @@
 # bash_logout -- Personal items to perform at logout.
 
+# Copyright (C) 2013-2014 Mike Barker
+
 # Mike Mike <mike@thebarkers.com>
 # April 13th, 2013
 
+# Cleanup when logging out
+if [ -n "$GNOME_KEYRING_PID" ]; then
+	killall gnome-keyring-daemon
+	killall -9 dbus-launch
+fi
--- bash_profile.d/sshagent.sh
+++ bash_profile.d/sshagent.sh
@@ -5,12 +5,15 @@
 # Author: Mike Barker <mike@thebarkers.com>
 # Created: October 24, 2014
 
-# initialize the ssh-agent script so that we can auto-login to ssh servers
-# on gnome systems that have the gnome-keyring-daemon installed, use it
-# otherwise use the ssh-agent command.
+# Initialize the ssh-agent script so that we can auto-login to ssh servers.
+# On gnome systems that have the gnome-keyring-daemon installed and are 
+# running X, setup the gnome-keyring-daemon to provide keymanagement.
+# To keep the shell from hanging when exiting, I export the GNOME_KEYRING_PID
+# and then in .bash_logout, I check for this environment variable and kill the
+# gnome-keyring-daemon and dbus-launch processes that have been spawned.
 
-if command gnome-keyring-daemon 2</dev/null; then
-    eval `gnome-keyring-daemon -s`
-else
-    eval `ssh-agent`
+eval `ssh-agent -s`
+if type gnome-keyring-daemon >/dev/null 2>&1 && [ -n "$DISPLAY" ]; then
+	eval `gnome-keyring-daemon -s`
+	export GNOME_KEYRING_PID
 fi
