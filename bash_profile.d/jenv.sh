cf3f454aaf3b44bef647bbdf123694f4024b7cf3
Mike Barker <mike@thebarkers.com>
2015-02-10 12:46:58 -0700

    Source code format changes

--- bash_profile.d/jenv.sh
+++ bash_profile.d/jenv.sh
@@ -2,6 +2,6 @@
 #            used to help you forget how to set the JAVA_HOME environment variable
 #            http://www.jenv.be/
 
-if type jenv > /dev/null 2>&1; then 
-	eval "$(jenv init -)"
+if type jenv > /dev/null 2>&1; then
+    eval "$(jenv init -)"
 fi
