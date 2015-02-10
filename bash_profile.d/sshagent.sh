346b8f6b7275beea9ea6e6be62184d894569519a
Mike Barker <mike@thebarkers.com>
2015-02-10 13:13:55 -0700

    Code now from github example. I changed the ssh-agent code to use the code snippits provided by github for setting up ssh-agent.

--- bash_profile.d/sshagent.sh
+++ bash_profile.d/sshagent.sh
@@ -11,24 +11,40 @@
 # * Bug: the ssh-agent was loaded at every login, this caused it to be left in mem.
 # * Gnome keyring setup moved to it's own file gnome-keyring-daemon.sh
 
-SSH_ENV="$HOME/.ssh/environment"
+# Note: ~/.ssh/environment should not be used, as it
+#       already has a different purpose in SSH.
+SSH_ENV="$HOME/.ssh/agent.env"
 
-function start_agent {
-    echo "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo succeeded
-    chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add;
+function agent_has_keys() {
+    ssh-add -l >/dev/null 2>&1
 }
 
-# Source SSH settings, if applicable
-if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
-    }
-else
-    start_agent;
+function agent_is_running() {
+    if [ "$SSH_AUTH_SOCK" ]; then
+	agent_has_keys || [ $? -eq 1]
+    else
+	false
+    fi
+}
+
+function agent_load_env() {
+    source "$SSH_ENV" > /dev/null
+}
+
+function agent_start() {
+    (umask 077; ssh-agent > "$SSH_ENV")
+    agent_load_env
+}
+
+if ! agent_is_running; then
+    agent_load_env
 fi
+
+if ! agent_is_running; then
+    agent_start
+    ssh-add
+elif ! agent_has_keys; then
+    ssh-add
+fi
+
+unset SSH_ENV
