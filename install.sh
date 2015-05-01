ca15e15c7dfd133f8d572f1644f93586d5ac9280
Mike Barker <mike@thebarkers.com>
2015-05-01 15:00:15 -0600

    Clean comments, force create symlinks

--- install.sh
+++ install.sh
@@ -4,21 +4,20 @@
 # Mike Barker <mike@thebarkers.com>
 # May 14th, 2013
 
-# 205.04.30 MRB
-# renamed from makelinks.sh to install.sh
+# 2015.04.30 MRB
+# * renamed from makelinks.sh to install.sh
+# * force create the sympolic links
 
 echo "$(basename $0)"
 echo "Make sympolic links in the users home directory to the bash config files and directories"
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-for FILE in bash* inputrc; do 
+for FILE in bash* inputrc; do
     echo "Processing file ${FILE}"
     if [ -a ~/.${FILE} ]; then
 	if ! [ -h ~/.${FILE} ]; then
 	    mv ~/.${FILE} ~/.${FILE}.backup
 	fi
     fi
-    if ! [ -h ~/.${FILE} ]; then
-	ln -sv ${DIR}/${FILE} ~/.${FILE}
-    fi
+    ln -fsv ${DIR}/${FILE} ~/.${FILE}
 done
