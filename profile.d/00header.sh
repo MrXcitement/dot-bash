9c5df94a8d2ae6fef269b1300189a9bc3803533b
Mike Barker <mike@thebarkers.com>
2015-05-06 09:38:18 -0600

    Only print DISPLAY if set and add blank lines.

--- profile.d/00header.sh
+++ profile.d/00header.sh
@@ -1,9 +1,14 @@
 # Display the bash version and DISPLAY setting
 if [ $BASH ]; then
     # Display the bash version and DISPLAY setting
+    echo
     printf "${BCyan}This is BASH ${BRed}${BASH_VERSION%.*}${BCyan}\n"
-    printf "DISPLAY on ${BRed}$DISPLAY${NC}\n"
+    if [ $DISPLAY ]; then
+	printf "DISPLAY on ${BRed}$DISPLAY${NC}\n"
+    fi
+
 else
+    echo
     echo $(sh --version)
     echo "DISPLAY on $DISPLAY"
 fi
@@ -11,10 +16,11 @@ fi
 # Display the date
 echo
 echo $(date)
-echo
 
 # Makes our day a bit more fun....
 if type fortune >/dev/null 2>&1; then
-    fortune -s
     echo
+    fortune -s
 fi
+
+echo
