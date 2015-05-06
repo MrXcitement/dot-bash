452e3438bd7e33a785477be0ff65f2cad5363d0d
Mike Barker <mike@thebarkers.com>
2015-05-06 10:02:07 -0600

    Fix color bleed after BASH info and sh/ash/dash does not have --version

--- profile.d/00header.sh
+++ profile.d/00header.sh
@@ -2,15 +2,17 @@
 if [ $BASH ]; then
     # Display the bash version and DISPLAY setting
     echo
-    printf "${BCyan}This is BASH ${BRed}${BASH_VERSION%.*}${BCyan}\n"
+    printf "${BBlue}This is BASH ${BRed}${BASH_VERSION%.*}${NC}\n"
     if [ $DISPLAY ]; then
-	printf "DISPLAY on ${BRed}$DISPLAY${NC}\n"
+	printf "${BBlue}DISPLAY on ${BRed}$DISPLAY${NC}\n"
     fi
 
 else
     echo
-    echo $(sh --version)
-    echo "DISPLAY on $DISPLAY"
+    echo "This is the 'sh' (Bourne Shell)"
+    if [ $DISPLAY ]; then
+	echo "DISPLAY on $DISPLAY"
+    fi
 fi
 
 # Display the date
