d21ae307a4164bfac8bcee40183b7f9743e0051e
Mike Barker <mike@thebarkers.com>
2015-05-12 10:26:17 -0600

    Added bash startup for .NET Version Manager

---
+++ profile.d/dnvm.bash
@@ -0,0 +1,10 @@
+# dnvm.bash --- .Net Version Manager command line tools
+
+# Mike Barker <mike@thebarkers.com>
+# May 12th, 2015
+
+
+# Only source the dnvm file if it exists.
+if $(type dnvm.sh); then
+    . dnvm.sh
+fi
