8de2ec558122a5ec50415efb71fee7c496308fad
Mike Barker <mike@thebarkers.com>
2015-05-13 10:36:44 -0600

    Foramting edits.

--- profile.d/amazon-aws.bash
+++ profile.d/amazon-aws.bash
@@ -1,11 +1,11 @@
-# #!/bin/bash
+#!/bin/bash
 
 # amazon-aws.bash --- Configure Amazon Web Services
 # If there is an aws credentials file, configure the aws cli to use
 # the credentials file and turn on tab completion support.
 
 # Author: Mike Barker <mike@thebarkers.com>
-# Created: May  8, 2015
+# Created: May 8th, 2015
 
 # History:
 # 2015.05.08
