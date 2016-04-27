b5dc8196426ea659295e4e41e74990febe63c1c0
Mike Barker <mike@thebarkers.com>
2016-04-26 19:15:01 -0600

    Fixup rvm config.

--- profile.d/rvm.sh
+++ profile.d/rvm.sh
@@ -1,5 +1,6 @@
 # rvm setup
-if [ -s "$HOME/.rvm/scripts/rvm" ]; then
-    PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-    . "$HOME/.rvm/scripts/rvm"
+
+if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then 
+	#export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+	source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 fi
