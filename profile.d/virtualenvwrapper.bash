d300883470b0ac8e03c4e6a1c97b63b6fc3323fc
Mike Barker <mike@thebarkers.com>
2016-09-29 12:05:43 -0600

    No longer location dependant
    Virtualenvwrapper initialization should now work on any os that
    it is installed on since it no longer looks in a specific dir
    but checks if it is in the path instead.

--- profile.d/virtualenvwrapper.bash
+++ profile.d/virtualenvwrapper.bash
@@ -1,8 +1,8 @@
 # virtualenv setup
-if [[ -x /usr/local/bin/virtualenv ]]; then
-    if [[ -x /usr/local/bin/virtualenvwrapper.sh ]]; then
+if [[ $(type -P virtualenv) ]]; then
+    if [[ $(type -P virtualenvwrapper.sh) ]]; then
         export WORKON_HOME=${HOME}/.virtualenvs
         export PROJECT_HOME=${HOME}/src
-        . /usr/local/bin/virtualenvwrapper.sh
+        . virtualenvwrapper.sh
     fi
 fi
