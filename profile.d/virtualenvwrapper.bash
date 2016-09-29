5982706e5fadc29a0e4f3f154d8e8e64640630bc
Mike Barker <mike@thebarkers.com>
2016-09-29 13:54:02 -0600

    pyenv support
    Initialize the pyenv tools including the pyenv virtualenvwrapper pluggin
    if it is installed.
    Modify virtualenvwrapper to not initialize if pyenv is installed

--- profile.d/virtualenvwrapper.bash
+++ profile.d/virtualenvwrapper.bash
@@ -1,6 +1,12 @@
-# virtualenv setup
-if [[ $(type -P virtualenv) ]]; then
-    if [[ $(type -P virtualenvwrapper.sh) ]]; then
+# virtualenvwrapper.bash --- initialize virtualenvwrapper
+# Mike Barker <mike@thebarkers.com>
+# September 29th 2016
+
+# Initalize the virtualenvwrapper tool if pyenv is not installed
+# If the pyenv tool is installed the virtualenvwrapper script will
+# crash bash.
+if [[ $(type -P virtualenvwrapper.sh) ]]; then
+    if [[ ! $(type -P pyenv ) ]]; then
         export WORKON_HOME=${HOME}/.virtualenvs
         export PROJECT_HOME=${HOME}/src
         . virtualenvwrapper.sh
