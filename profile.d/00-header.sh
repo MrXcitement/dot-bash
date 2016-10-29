7215dfcc67e474890ee1479c0a5f771ae7f5221e
Mike Barker <mike@thebarkers.com>
2016-10-29 12:44:39 -0600

    Tweak the login header message
    Fixed checking for bash or other shell
    Added cowsay support if it is installed

--- profile.d/00-header.sh
+++ profile.d/00-header.sh
@@ -1,5 +1,5 @@
 # Display the bash version and DISPLAY setting
-if [ $BASH ]; then
+if [ "$0" == "-bash" ] || [ "$0" == "bash" ]; then
     # Display the bash version and DISPLAY setting
     echo
     printf "${BBlue}This is BASH ${BRed}${BASH_VERSION%.*}${NC}\n"
@@ -9,7 +9,7 @@ if [ $BASH ]; then
 
 else
     echo
-    echo "This is the 'sh' (Bourne Shell)"
+    echo "This is the $0 (Bourne Shell)"
     if [ $DISPLAY ]; then
 	echo "DISPLAY on $DISPLAY"
     fi
@@ -22,7 +22,19 @@ echo $(date)
 # Makes our day a bit more fun....
 if type fortune >/dev/null 2>&1; then
     echo
-    fortune -s
+    if type cowsay >/dev/null 2>&1; then
+        if [ "$(uname)" == "Darwin" ]; then
+            _COWPATH=/usr/local/Cellar/cowsay/3.04/share/cows
+            _COWFILE=$(ls ${_COWPATH}/*cow | gshuf -n1)
+        else
+            _COWPATH=/usr/share/cowsay/cows
+            _COWFILE=$(ls ${_COWPATH}/*cow | shuf -n1)
+        fi
+        [ $DEBUG ] && echo "fortune -s | cowsay -f ${_COWFILE}"
+        fortune -s | cowsay -f ${_COWFILE}
+    else
+        fortune -s
+    fi
 fi
 
 echo
