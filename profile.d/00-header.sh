1234db8ba64fd13e4da2b6e68344640b8663b2f2
Mike Barker <mike@thebarkers.com>
2016-11-12 21:42:36 -0700

    Split header into two files
    Moved fortune/cowsay functionality from header to
    to header-fortune file.

--- profile.d/00-header.sh
+++ profile.d/00-header.sh
@@ -1,40 +1,17 @@
 # Display the bash version and DISPLAY setting
 if [ "$0" == "-bash" ] || [ "$0" == "bash" ]; then
     # Display the bash version and DISPLAY setting
-    echo
     printf "${BBlue}This is BASH ${BRed}${BASH_VERSION%.*}${NC}\n"
     if [ $DISPLAY ]; then
-	printf "${BBlue}DISPLAY on ${BRed}$DISPLAY${NC}\n"
+        printf "${BBlue}DISPLAY on ${BRed}$DISPLAY${NC}\n"
     fi
 
 else
-    echo
     echo "This is the $0 (Bourne Shell)"
     if [ $DISPLAY ]; then
-	echo "DISPLAY on $DISPLAY"
+        echo "DISPLAY on $DISPLAY"
     fi
 fi
 
 # Display the date
-echo
 echo $(date)
-
-# Makes our day a bit more fun....
-if type fortune >/dev/null 2>&1; then
-    echo
-    if type cowsay >/dev/null 2>&1; then
-        if [ "$(uname)" == "Darwin" ]; then
-            _COWPATH=/usr/local/Cellar/cowsay/3.04/share/cows
-            _COWFILE=$(ls ${_COWPATH}/*cow | gshuf -n1)
-        else
-            _COWPATH=/usr/share/cowsay/cows
-            _COWFILE=$(ls ${_COWPATH}/*cow | shuf -n1)
-        fi
-        [ $DEBUG ] && echo "fortune -s | cowsay -f ${_COWFILE}"
-        fortune -s | cowsay -f ${_COWFILE}
-    else
-        fortune -s
-    fi
-fi
-
-echo
---
+++ profile.d/10-header-fortune.sh
@@ -0,0 +1,78 @@
+# fotune.bash --- print out a random fortune, use cowsay if installed
+
+# This script will check for the fortune program and use it to
+# retrieve a random fortune to be displayed.
+
+# If the cowsay program is installed, cowsay will be used to print
+# the fortune with a random cow selected.
+
+# If a cow file is provided, then it will be used instead of a random
+# cow file instead.
+
+# If the cow file randomly selected or requested is the sodomized.cow
+# file, this script will replace it with the default.cow file.
+
+# parameters:
+# -c <cow file> or --cowfile <cow file>
+
+# Mike Barker <mike@thebarkers.com>
+# November 11th, 2016
+
+clean_cowfile() {
+    cowfile=$1
+    if [ "$1" == "sodomized.cow" ]; then
+        cowfile="default.cow"
+    fi
+    echo ${cowfile}
+}
+
+get_shuffle() {
+    if [ "$(uname)" == "Darwin" ]; then
+        shuffle=gshuf
+    else
+        shuffle=shuf
+    fi
+    echo ${shuffle}
+}
+get_cowpath() {
+    if [ "$(uname)" == "Darwin" ]; then
+        cowpath=/usr/local/Cellar/cowsay/3.04/share/cows
+    else
+        cowpath=/usr/share/cowsay/cows
+    fi
+    echo ${cowpath}
+}
+
+get_cowfile() {
+    if [ "$1" == "" ]; then
+        cowfile=$(ls $(get_cowpath)/*cow | \
+                  xargs -n1 basename | \
+                  $(get_shuffle) -n1)
+    fi
+    echo $(clean_cowfile ${cowfile})
+}
+
+# parse commandline
+_COWFILE="" # Need a cowfile
+OPTIND=1    # Index into options, reset to start at first option
+while getopts ":c:" opt; do
+    case $opt in
+        c)
+            _COWFILE=$OPTARG
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+[ "$1" = "--" ] && shift
+
+if type fortune >/dev/null 2>&1; then
+    echo
+    if type cowsay >/dev/null 2>&1; then
+        [ $DEBUG ] && echo "fortune -s | cowsay -f $(get_cowpath)/$(get_cowfile)"
+        fortune -s | cowsay -f $(get_cowpath)/$(get_cowfile)
+    else
+        [ $DEBUG ] && echo "fortune -s"
+        fortune -s
+    fi
+    echo
+fi
