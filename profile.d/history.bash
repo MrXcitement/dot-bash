8ada2a4f2061605ab65e0ec1570b8391122bbcb9
Mike Barker <mike@thebarkers.com>
2017-03-24 09:33:49 -0600

    Bash history modifications
    Use a space followed by a sensitive command to not add to history
    Ignore dups and remove any dups from the history
    Ignore all single and double letter commands

--- profile.d/history.bash
+++ profile.d/history.bash
@@ -1,6 +1,7 @@
 # History configuration
 export HISTSIZE=1000
-export HISTIGNORE="&b:[bf]g:exit"
-#export HISTIGNORE="&:bg:fg:ll:h"
-export HISTTIMEFORMAT="$(echo ${BCyan})[%d/%m %H:%M:%S]$(echo ${NC}) "
-export HISTCONTROL=ignoredups
+export HISTIGNORE="&:?:??:exit:pwd:clear"
+export HISTTIMEFORMAT="[%d/%m %H:%M:%S] "
+export HISTCONTROL=ignoreboth:erasedups
+
+alias h=history
