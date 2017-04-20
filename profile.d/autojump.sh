dd805aa7d9362303d4cedcc2c6b0e8bf8899215d
Mike Barker <mike@thebarkers.com>
2017-04-20 09:42:55 -0600

    Added autojump configuration

---
+++ profile.d/autojump.sh
@@ -0,0 +1,5 @@
+# autojump.sh --- configure autojump 
+# A cd command that learns - easily navigate directories from the command line
+# https://github.com/wting/autojump
+
+[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
