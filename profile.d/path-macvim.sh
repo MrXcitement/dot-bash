d929a907906c48c0a8500958ce984f862cf30155
Mike Barker <mike@thebarkers.com>
2017-10-01 11:19:08 -0600

    Add path-macvim.sh
    Script to prepend the macvim bin folder, if it exists, to the path

---
+++ profile.d/path-macvim.sh
@@ -0,0 +1,9 @@
+# path-macvim.sh --- Prepend the macvim bin folder to the path
+#
+# Mike Barker <mike@thebarkers.com>
+# October 1, 2017
+
+MACVIM_PATH="/Applications/MacVim.app/Contents/bin"
+if [ -d $MACVIM_PATH ]; then
+    PATH=$MACVIM_PATH:$PATH
+fi
