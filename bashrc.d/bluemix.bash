88185abd78a9b32dc8dc23583e87c1465f651b09
Mike Barker <mike@thebarkers.com>
2017-10-26 16:28:54 -0600

    Bluemix bash autocomletion

---
+++ bashrc.d/bluemix.bash
@@ -0,0 +1,13 @@
+# bluemix.bash --- Configure the homebrew package system
+
+# Author: Mike Barker <mike@thebarkers.com>
+# Created: October 26th, 2017
+
+# History:
+# 2017-10-26
+# * First release.
+
+# bx bash autocompletion
+if [[ $(command -v bx) ]]; then
+    source /usr/local/Bluemix/bx/bash_autocomplete
+fi
