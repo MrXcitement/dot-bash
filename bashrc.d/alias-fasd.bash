c2d7ccd4392282224c2e450ed5b54798915062ec
Mike Barker <mike@thebarkers.com>
2019-03-11 10:34:40 -0600

    Add alias-fasd.bash

---
+++ bashrc.d/alias-fasd.bash
@@ -0,0 +1,11 @@
+# alias-fasd.bash --- aliases for the 'fast' cli productivity booster
+# https://github.com/clvv/fasd
+
+# Mike Barker <mike@thebarkers.com>
+# March 11th, 2019
+
+alias v='f -e vim'      # quick opening files with vim
+alias o='a -e open'     # 'open' files in associated app
+
+_fasd_bash_hook_cmd_complete v o
+
