c300fffc19c9b571a0f9a0f1f6ca9bf6802c9d64
Mike Barker <mike@thebarkers.com>
2019-03-12 17:43:39 -0600

    Modified alias-fasd.bash
    Only call the completion hook if it is defined.

--- bashrc.d/alias-fasd.bash
+++ bashrc.d/alias-fasd.bash
@@ -7,5 +7,6 @@
 alias v='f -e vim'      # quick opening files with vim
 alias o='a -e open'     # 'open' files in associated app
 
-_fasd_bash_hook_cmd_complete v o
+FASD_BASH_HOOK=_fasd_bash_hook_cmd_complete
+[ $(type -t ${FASD_BASH_HOOK}) ] && "${FASD_BASH_HOOK}" v o
 
