a7901e1ad1e1b89652183498f023d80ba87b8154
Mike Barker <mike@thebarkers.com>
2019-03-14 16:18:59 -0600

    Modify profile.d/fasd.bash
    Gaurd against fasd not installed.

--- profile.d/fasd.bash
+++ profile.d/fasd.bash
@@ -5,5 +5,11 @@
 # Mike Barker <mike@thebarkers.com>
 # March 5th, 2019
 
-eval "$(fasd --init auto)"
+# History
+# 2019-03-14 MRB
+# Gaurd against the fasd program not installed.
+
+if [[ "$(command -v fasd)" ]]; then
+    eval "$(fasd --init auto)"
+fi
 
