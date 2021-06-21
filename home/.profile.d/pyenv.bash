7557290b8be6c20e81795d8c4b82d0794b1600fd
Mike Barker <mike@thebarkers.com>
2021-06-21 10:43:42 -0600

    Update pyenv.bash

--- home/.profile.d/pyenv.bash
+++ home/.profile.d/pyenv.bash
@@ -4,18 +4,18 @@
 # September 29th 2016
 
 # Check if pyenv is installed, then initialize it
-if [[ $(type -P "pyenv") ]]; then
+if [[ $(command -v "pyenv") ]]; then
     export PYENV_ROOT="${HOME}/.pyenv"
     pathprepend "${PYENV_ROOT}/bin"
     eval "$(pyenv init -)"
 
     # Initialize pyenv's virtualenv plugin, if installed
-    if [[ $(type -P pyenv-virtualenv-init ) ]]; then
+    if [[ $(command -v pyenv-virtualenv-init ) ]]; then
         eval "$(pyenv virtualenv-init -)"
     fi
 
     # Initalize pyenv's virtualenvwrapper tool only if pyenv-virtualenvwrapper is installed
-    if [[ $(type -P pyenv-sh-virtualenvwrapper ) ]]; then
+    if [[ $(command -v pyenv-sh-virtualenvwrapper ) ]]; then
         export WORKON_HOME=${HOME}/.virtualenvs
         export PROJECT_HOME=${HOME}/src
         pyenv virtualenvwrapper
